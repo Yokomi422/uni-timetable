df55eda1b11c787876492b837edeb67b5e2af144
Rei Tsukada <rayzou1120@icloud.com>
2024-05-07 21:51:48 +0900

    signoutとerrorを分岐に追加

--- frontend/auth.config.ts
+++ frontend/auth.config.ts
@@ -3,19 +3,22 @@ import type { NextAuthConfig } from 'next-auth';
 export const authConfig = {
   pages: {
     signIn: '/login',
+    signOut: '/signout',
+    error: '/error',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnDashboard = nextUrl.pathname.startsWith('/home');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/home', nextUrl));
       }
       return true;
     },
   },
+  // TODO あとでoptionを追加
   providers: [],
 } satisfies NextAuthConfig;
