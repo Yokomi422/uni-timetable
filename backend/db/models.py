756ceb3513c48276699ad7c5e814d781f39d0397
Rei Tsukada <rayzou1120@icloud.com>
2024-05-14 23:49:23 +0900

    modelの型を決定

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,11 +1,27 @@
+"""
+サンプルデータを準備するときは、
+以下のコマンドを実行してください。
+```
+python fix_copied_str.py [ファイル名] | pbcopy
+```
+"""
+
 from typing import List, Optional
 
 from sqlmodel import Field, Relationship, SQLModel
 
 
+class Faculty(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    departments: List["Department"] = Relationship(back_populates="faculty")
+
+
 class Department(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
+    faculty_id: Optional[int] = Field(default=None, foreign_key="faculty.id")
+    faculty: Optional[Faculty] = Relationship(back_populates="departments")
     classes: List["Class"] = Relationship(back_populates="department")
 
 
