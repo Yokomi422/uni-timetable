53077ba96d6852ef4028cab3711e14a089743a2c
Rei Tsukada <rayzou1120@icloud.com>
2024-05-20 17:52:42 +0900

    情報の型を定義

---
+++ backend/scripts/scraping/type.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+
+class Urls(BaseModel):
+    engineering: str
+    science: str
+
+
+urls = Urls(
+    engineering="https://catalog.he.u-tokyo.ac.jp/result?q=&type=ug&faculty_id=3&facet=%7B%22semester_codes%22%3A%5B%22S1%22%2C%22S2%22%5D%7D&page=",
+    science="https://catalog.he.u-tokyo.ac.jp/result?q=&type=ug&faculty_id=5&facet=%7B%7D&page=",
+)
+
+
+# インスタンスはimportできないため
+def get_urls():
+    return urls
+
+
+class ClassAttributes(BaseModel):
+    name: str
+    teacher: str
+    semester: str
+    credits: int
+    period: str
+    plan: str
+    how_grading: str
+    caution: str
